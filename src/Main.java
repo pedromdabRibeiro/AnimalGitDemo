3bdc9550665fd8013068b29a7355e2bb4a4b0ffc
Pedro <pmd.ribeiro@campus.fct.unl.pt>
2021-09-30 16:57:55 +0100

    Initial version of our main class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author Pedro Manuel Ribeiro, n. 55921
+ * Demo for the Git lab on Software Engineering
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Animal git demo");
+    }
+}
